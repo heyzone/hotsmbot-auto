5937f071e6dc50c022e55e8d4ac6899a9427c44e
heyzone <harrywujy520@gmail.com>
2025-09-14 13:27:30 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from playwright.sync_api import sync_playwright, TimeoutError as PlaywrightTimeo
 from datetime import datetime
 
 # --- 配置项 (保持不变) ---
-SERVER_URL = "https://panel.sillydev.co.uk/server/9425fb98"
-LOGIN_URL = "https://panel.sillydev.co.uk/auth/login"
+SERVER_URL = "https://panel.hostmybot.net/server/cadc7900"
+LOGIN_URL = "https://panel.hostmybot.net/auth/login"
 COOKIE_NAME = "remember_web_59ba36addc2b2f9401580f014c7f58ea4e30989d"
 TASK_TIMEOUT_SECONDS = 300
 
